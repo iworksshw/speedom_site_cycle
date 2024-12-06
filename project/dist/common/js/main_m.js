1c50188d528f3a7cdaf2fd66a30f30b59c1238a7
meameng <suji0220@naver.com>
2024-12-06 17:01:10 +0900

    20241206 jsj

--- project/dist/common/js/main_m.js
+++ project/dist/common/js/main_m.js
@@ -13,14 +13,28 @@ document.addEventListener("DOMContentLoaded", function () {
     // 이벤트 배너 슬라이드(전체)
     var swiper = new Swiper(".event01Swiper", {
         slidesPerView: "auto",
-        spaceBetween: 30,
+        spaceBetween: 12,
     });
     var swiper = new Swiper(".event02Swiper", {
         slidesPerView: "auto",
-        spaceBetween: 30,
+        spaceBetween: 12,
     });
     var swiper = new Swiper(".event03Swiper", {
         slidesPerView: "auto",
-        spaceBetween: 30,
+        spaceBetween: 12,
     });
+
+    // 이벤트 탭
+    const tabBtns = document.querySelectorAll(".eventArea .tabBtn");
+    const tabConts = document.querySelectorAll(".eventArea .tabCont");
+
+    tabBtns.forEach(function(tabBtn, index){
+        tabBtn.addEventListener("click", function(){
+            tabBtns.forEach(btn => btn.classList.remove('on'));
+            tabConts.forEach(content => content.classList.remove('on'));
+
+            tabBtn.classList.add("on");
+            tabConts[index].classList.add("on");
+        })
+    })
 });
